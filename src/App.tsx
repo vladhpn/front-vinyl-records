43f5da956955d3f42c602cc012b224b61cb3a199
Владислав Гапон <vl.gapon@temabit.com>
2023-12-19 16:01:02 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@nextui-org/react';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-600 underline">
+        Hello world!
+      </h1>
+      <Button color="primary">Button</Button>
+    </>
+  );
+}
+
+export default App;
