ca92abf32c209602e5973660fb1f8807d6b9e15e
Anton Vasyliev <anton12vasyliev@gmail.com>
2023-12-20 20:50:20 +0000

    fix delete coments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
 
-// tailwind.config.js
 import { nextui } from '@nextui-org/react';
 
 export default {
