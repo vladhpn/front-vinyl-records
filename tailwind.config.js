55d61acefb2e7ab8689f1c3a084b7d1193c4a9bb
ant0nvasyliev <anton12vasyliev@gmail.com>
2023-12-20 12:22:22 +0000

    container+fonts+colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,28 @@ export default {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        sm: '16px',
+        md: '24px',
+        xl: '40px',
+        '2xl': '80px',
+      },
+    },
+    fontFamily: {
+      sans: 'Inter, sans-serif',
+    },
+    extend: {
+      colors: {
+        lightBlue: '#3498DB',
+        darkBlue: '#24478F',
+        red: '#E74C3C',
+        green: '#2ECC71',
+        lightGray: '#EDEDED',
+      },
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
