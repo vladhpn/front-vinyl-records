a80fbb78211c02b34bc0f9e3879fac869c692552
Anton Vasyliev <anton12vasyliev@gmail.com>
2024-01-07 10:30:56 +0000

    fix/ route and home path

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: '/home',
+        path: '/',
         element: <HomePage />,
       },
     ],
