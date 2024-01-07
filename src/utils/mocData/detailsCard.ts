b97b09e81d285a584e91ad64cc33d8f88d28cb3d
Anton Vasyliev <anton12vasyliev@gmail.com>
2024-01-07 17:46:32 +0000

    feature/cards-mock-data

---
+++ src/utils/mocData/detailsCard.ts
@@ -0,0 +1,19 @@
+export const detailCardData = {
+  format: 'Vinyl',
+  description:
+    "'2023 marks the 10th anniversary of Daft Punk's multi Grammy winning hit record 'Random Access Memories', including 'Get Lucky', 'Instant Crush', 'Lose Yourself to Dance' and featuring Pharrell Willams, Nile Rodgers, Julien Casablancas, Paul Williams, Panda Bear and Todd Edwards. This special expanded edition includes 35 minutes of unreleased music (Demo and Studio Outtakes).'",
+  price: '75.64',
+  artist: 'Daft Punk',
+  album: 'Random Access Memories',
+  diameter: 12, // вибір між 3-ма варіантами - 7, 10, 12.
+  rpm: 33, // вибір між 3-ма варіантами - 33, 45, 78.
+  genre: 'Dance',
+  style: 'House',
+  recordLabel: 'Sony Music CMG',
+  weight: 140, // або 180
+  condition: 'Mint ', // відноситься до платівки і обкладинки: Mint, Near Mint, Very Good Plus, Very Good, Good, Fair, Poor.
+  coverCondition: 'Mint ',
+  images: 'https://i.ebayimg.com/images/g/MnwAAOSwJZ9lg1f2/s-l1600.jpg',
+  id: '1',
+  releaseYear: 2013, // додано рік випуску
+};
